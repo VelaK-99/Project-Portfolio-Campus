1c729b6bf1311797de29fc07b3c3cc669cd522cd
victorpbk <victorboukarim1@gmail.com>
2025-04-08 23:39:15 -0400

    playerController done. No shooting yet

---
+++ Unity Project/Assets/Scripts/playerController.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    [SerializeField] LayerMask ignoreLayer;
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpMax;
+    [SerializeField] int gravity;
+
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootRate;
+
+
+    int jumpCount;
+
+    float shootTimer;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    bool isSprinting;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        Sprint();
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.green);
+    }
+
+    void Movement()
+    {
+
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) +
+                 (Input.GetAxis("Vertical") * transform.forward);
+
+
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        Jump();
+
+        playerVel.y -= gravity * Time.deltaTime;
+        controller.Move(playerVel * Time.deltaTime);
+
+        shootTimer += Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+}
+
