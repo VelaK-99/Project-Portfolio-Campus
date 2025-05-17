519f9e02e274d7e349fe263a8e2e4dfa1958c23d
Justin Abreu <jabreu1@student.fullsail.edu>
2025-05-17 19:20:53 -0400

    Player rage and updated enemy gunner script

--- Unity Project/Assets/Scripts/MutatedEnemyGunner.cs
+++ Unity Project/Assets/Scripts/MutatedEnemyGunner.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class MutatedEnemyGunner : MonoBehaviour
 {
@@ -10,51 +11,132 @@ public class MutatedEnemyGunner : MonoBehaviour
     [SerializeField] float fireRate;
     [SerializeField] float moveSpeed;
     [SerializeField] int health;
+    [SerializeField] int lowHealthThreshold;
+    [SerializeField] float retreatDistance;
+    [SerializeField] int burstCount;
+    [SerializeField] float burstDelay;
     [SerializeField] Animator animator;
+    [SerializeField] Transform[] patrolPoints;
+
+    enum State { Patrolling, Chasing, Shooting, Retreating, Dead }
+    State mCurrentState;
 
     NavMeshAgent mAgent;
     float mNextFireTime;
+    int mCurrentPatrolIndex;
 
     void Start()
     {
         mAgent = GetComponent<NavMeshAgent>();
         mAgent.speed = moveSpeed;
+        mCurrentState = State.Patrolling;
+        GoToNextPatrolPoint();
     }
 
     void Update()
     {
-        if (player == null) return;
+        if (mCurrentState == State.Dead || player == null) return;
 
         float distance = Vector3.Distance(transform.position, player.position);
 
-        if (distance <= detectionRange)
+        if (health <= lowHealthThreshold)
         {
-            mAgent.SetDestination(player.position);
-            animator.SetBool("isMoving", true);
-
-            Vector3 direction = (player.position - transform.position).normalized;
-            direction.y = 0;
-            transform.forward = direction;
-
-            if (Time.time >= mNextFireTime)
-            {
-                Shoot();
-                mNextFireTime = Time.time + 1f / fireRate;
-            }
-
-            animator.SetBool("isShooting", true);
+            RetreatFromPlayer();
+            return;
         }
-        else
+
+        switch (mCurrentState)
         {
-            mAgent.ResetPath();
-            animator.SetBool("isMoving", false);
-            animator.SetBool("isShooting", false);
+            case State.Patrolling:
+                Patrol();
+                if (distance <= detectionRange)
+                    mCurrentState = State.Chasing;
+                break;
+
+            case State.Chasing:
+                if (distance > detectionRange)
+                {
+                    mCurrentState = State.Patrolling;
+                    animator.SetBool("isMoving", true);
+                    GoToNextPatrolPoint();
+                    return;
+                }
+
+                mAgent.SetDestination(player.position);
+                animator.SetBool("isMoving", true);
+
+                if (distance <= mAgent.stoppingDistance + 2f)
+                {
+                    mCurrentState = State.Shooting;
+                    animator.SetBool("isMoving", false);
+                }
+                break;
+
+            case State.Shooting:
+                transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+
+                if (Time.time >= mNextFireTime)
+                {
+                    StartCoroutine(FireBurst());
+                    mNextFireTime = Time.time + 1f / fireRate;
+                }
+
+                if (distance > detectionRange)
+                {
+                    mCurrentState = State.Chasing;
+                }
+                break;
+
+            case State.Retreating:
+                RetreatFromPlayer();
+                break;
         }
     }
 
-    void Shoot()
+    void Patrol()
     {
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        if (patrolPoints.Length == 0) return;
+
+        if (!mAgent.pathPending && mAgent.remainingDistance < 0.5f)
+        {
+            GoToNextPatrolPoint();
+        }
+    }
+
+    void GoToNextPatrolPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+
+        mAgent.destination = patrolPoints[mCurrentPatrolIndex].position;
+        mCurrentPatrolIndex = (mCurrentPatrolIndex + 1) % patrolPoints.Length;
+        animator.SetBool("isMoving", true);
+    }
+
+    IEnumerator FireBurst()
+    {
+        animator.SetBool("isShooting", true);
+
+        for (int i = 0; i < burstCount; i++)
+        {
+            Vector3 spread = firePoint.forward;
+            spread += firePoint.right * Random.Range(-0.1f, 0.1f);
+            spread += firePoint.up * Random.Range(-0.1f, 0.1f);
+
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.LookRotation(spread));
+            yield return new WaitForSeconds(burstDelay);
+        }
+
+        animator.SetBool("isShooting", false);
+    }
+
+    void RetreatFromPlayer()
+    {
+        Vector3 direction = (transform.position - player.position).normalized;
+        Vector3 retreatPosition = transform.position + direction * retreatDistance;
+
+        mAgent.SetDestination(retreatPosition);
+        mCurrentState = State.Retreating;
+        animator.SetBool("isMoving", true);
     }
 
     public void TakeDamage(int damage)
@@ -68,6 +150,7 @@ public class MutatedEnemyGunner : MonoBehaviour
 
     void Die()
     {
+        mCurrentState = State.Dead;
         animator.SetTrigger("die");
         mAgent.enabled = false;
         this.enabled = false;
