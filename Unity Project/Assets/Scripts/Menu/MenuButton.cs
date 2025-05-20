6fcb94f90fa014cf83ee6555e5078239d784800d
Valenco Silva <101263360+ValencoSilva@users.noreply.github.com>
2025-05-20 02:33:49 -0400

    upd Audio feedback on the menus

--- Unity Project/Assets/Scripts/Menu/MenuButton.cs
+++ Unity Project/Assets/Scripts/Menu/MenuButton.cs
@@ -4,9 +4,9 @@ using UnityEngine.SceneManagement;
 public class MenuButton : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-    [SerializeField] private GameObject Start_button;
     [SerializeField] private GameObject painelMenu;
     [SerializeField] private GameObject painelSettings;
+    public AudioSource audioSource;
 
 
 
@@ -16,26 +16,36 @@ public class MenuButton : MonoBehaviour
         Cursor.visible = true;
     }
 
+    public void ClickSound()
+    {
+        if (audioSource != null)
+            audioSource.Play();
+    }
+
 
     public void Play()
     {
+        ClickSound();
         SceneManager.LoadScene(sceneName);
     }
 
     public void OpenSetting()
     {
+        ClickSound();
         painelMenu.SetActive(false);
         painelSettings.SetActive(true);
     }
 
     public void CloseSetting()
     {
+        ClickSound();
         painelSettings.SetActive(false);
         painelMenu.SetActive(true);
     }
 
     public void Quit()
     {
+        ClickSound();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
--- Unity Project/Assets/Scripts/Movement/buttonFunctions.cs
+++ Unity Project/Assets/Scripts/Movement/buttonFunctions.cs
@@ -4,35 +4,47 @@ using UnityEngine.UIElements;
 
 public class buttonFunctions : MonoBehaviour
 {
-    
+    public AudioSource audioSource;
+
+    public void ClickSound()
+    {
+        if (audioSource != null)
+            audioSource.Play();
+    }
     public void resume()
     {
+        
         gameManager.instance.stateUnpause();
+
     }
 
     public void restart()
     {
+       
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         gameManager.instance.stateUnpause();
+        ClickSound();
     }
 
     public void respawn()
     {
+       
         gameManager.instance.playerScript.spawnPlayer();
         gameManager.instance.stateUnpause();
     }
     public void OpenSetting()
     {
-       
+        ClickSound();
         gameManager.instance.painelSettings.SetActive(true);
     }
     public void CloseSetting()
     {
-
+        ClickSound();
         gameManager.instance.painelSettings.SetActive(false);
     }
     public void Quit()
     {
+        ClickSound();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
